5458cb2a08ce88e39537847faf94e5286d778f47
zothedev <64105313+zothedev@users.noreply.github.com>
2025-05-13 14:55:27 -0400

    Add the ability to loop over myLibrary and display
    We can now loop over the array of books (myLibrary)
    and display info about each one.

--- script.js
+++ script.js
@@ -8,18 +8,44 @@ function Book(title, author, pages, read) {
     this.read = read
 
     // methods
-    this.info = function() {
+    let info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
     }
 }
 
-// let myBook = new Book("Hatchet", "theAuthor", 42, "already read");
-// console.log(myBook.info());
-
 function addBookToLibrary(title, author, pages, read) {
     // take params, create a book then store it in the array
-    let x = new Book(this, title, author, pages, read);
+    let x = new Book(title, author, pages, read);
     x.id = crypto.randomUUID();
     myLibrary.push(x);
 }
 
+addBookToLibrary("testTitle", "testAuthor", 43, "already read");
+addBookToLibrary("testTi44tle", "testAuthor", 43, "already read");
+
+// create a var referencing the .cards container
+const cards = document.querySelector('.cards');
+
+// displaying books on screen
+
+// for each element in the array
+for (const element of myLibrary) {
+
+    // create a card for it
+    const card = document.createElement('div');
+    // FIX: adding an ID to the card (to create the border) works, but not when I tried adding a class.
+    card.setAttribute('id', 'testy');
+    // add that card to the dom as a child of .cards
+    cards.appendChild(card);
+
+    // for each property of the current book object
+    for (const property in element) {
+        // create a new <p> to hold this properties info
+        const bookItem = document.createElement('p');
+        // update the text contents of the <p>
+        bookItem.textContent = `${property}: ${element[property]}`;
+        // add the bookItem to the current card
+        card.appendChild(bookItem);
+    }
+    
+}
