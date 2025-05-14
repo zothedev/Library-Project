807cdd8d25ec7db2950b92cfce9e7d857edde1fc
zothedev <64105313+zothedev@users.noreply.github.com>
2025-05-14 12:08:25 -0400

    Added a class to newly-created books.
    Also formatted the property names in the card
    element by capitalizing the first letter of each
    property.

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     // take params, create a book then store it in the array
     let x = new Book(title, author, pages, read);
-    x.id = crypto.randomUUID();
+    x.ID = crypto.randomUUID();
     myLibrary.push(x);
 }
 
@@ -33,8 +33,8 @@ for (const element of myLibrary) {
 
     // create a card for it
     const card = document.createElement('div');
-    // FIX: adding an ID to the card (to create the border) works, but not when I tried adding a class.
-    card.setAttribute('id', 'testy');
+    // add the card class to the card 
+    card.classList.add('card');
     // add that card to the dom as a child of .cards
     cards.appendChild(card);
 
@@ -42,8 +42,8 @@ for (const element of myLibrary) {
     for (const property in element) {
         // create a new <p> to hold this properties info
         const bookItem = document.createElement('p');
-        // update the text contents of the <p>
-        bookItem.textContent = `${property}: ${element[property]}`;
+        // update the text contents of the <p> (while capitalizing the first letter of the property)
+        bookItem.textContent = `${property[0].toUpperCase() + property.substring(1)}: ${element[property]}`;
         // add the bookItem to the current card
         card.appendChild(bookItem);
     }
